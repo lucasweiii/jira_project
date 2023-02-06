eddbe2b70df13a4ef962e161d8888e4af35b4ecb
yukii <rweiyy@gmail.com>
2023-02-06 14:47:42 +0800

    🔧 build: setPretterJsonserver

---
+++ src/screens/login-register/Index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const LoginRegisterScreen = () => {
+  return <div>Login-register</div>;
+};
+export default LoginRegisterScreen;
---
+++ src/screens/project-list/Index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SearechPanel from "./SearchPanel";
+import List from "./List";
+const ProjectListScreen = () => {
+  return (
+    <>
+      <div>project-list</div>
+      <List />
+      <SearechPanel />
+    </>
+  );
+};
+export default ProjectListScreen;
