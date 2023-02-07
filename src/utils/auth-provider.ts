be68a37c3718ba4d67cbe88608cbbf76b3b12414
yukii <rweiyy@gmail.com>
2023-02-07 10:55:55 +0800

    ✨ feat: add:auth provider fn

---
+++ src/utils/auth-provider.ts
@@ -0,0 +1,56 @@
+// 在真实环境中,如果使用firebase这类第三方auth服务的话,本文件不需要开发者开发
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+  token: string;
+}
+//获取环境变量里存的url
+const apiUrl = process.env.REACT_APP_API_URL;
+
+const localStorageKey = "__auth_provider_token__";
+
+// 获取token
+export const getToken = () => window.localStorage.getItem(localStorageKey);
+
+//操作请求返回 设置token 并返回
+export const handleUserResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+//login 函数
+export const login = (data: { username: string; password: string }) => {
+  fetch(`${apiUrl}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res: Response) => {
+    if (res.ok) {
+      return handleUserResponse(await res.json());
+    }
+  });
+};
+
+//register 函数
+export const register = (data: { username: string; password: string }) => {
+  fetch(`${apiUrl}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (res: Response) => {
+    if (res.ok) {
+      return handleUserResponse(await res.json());
+    }
+  });
+};
+
+//登出 函数
+export const logout = window.localStorage.clear();
